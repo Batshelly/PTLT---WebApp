3ca7321a44087718fd65a5f7db7ec22919cefb2c
Yangkiiii <146529283+Yangkiiii@users.noreply.github.com>
2025-11-21 02:05:42 +0800

    SAR 3.9.3

--- PTLT_App/migrations/0010_add_missing_attendance_fields.py
+++ PTLT_App/migrations/0010_add_missing_attendance_fields.py
@@ -1,14 +1,12 @@
 from django.db import migrations, models
 
-
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('PTLT_App', '0010_alter_attendancerecord_status'),  # ← Your last local migration
+        ('PTLT_App', 'your_last_migration_name_here'),
     ]
 
     operations = [
-        # REMOVE from ClassSchedule (Railway has these, we're dropping them)
         migrations.RemoveField(
             model_name='classschedule',
             name='professor_time_in',
@@ -16,19 +14,6 @@ class Migration(migrations.Migration):
         migrations.RemoveField(
             model_name='classschedule',
             name='professor_time_out',
-        ),
-        
-        # ADD to AttendanceRecord
-        migrations.AddField(
-            model_name='attendancerecord',
-            name='time_in',
-            field=models.TimeField(verbose_name='Time In', default='00:00'),
-            preserve_default=False,
-        ),
-        migrations.AddField(
-            model_name='attendancerecord',
-            name='time_out',
-            field=models.TimeField(verbose_name='Time Out', null=True, blank=True),
         ),
         migrations.AddField(
             model_name='attendancerecord',
