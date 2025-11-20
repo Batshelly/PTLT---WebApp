feaa483dc74f732cb3c25999bd720f4dd5cbdc01
Yangkiiii <146529283+Yangkiiii@users.noreply.github.com>
2025-11-21 00:54:52 +0800

    Create 0010_add_missing_attendance_fields.py

---
+++ PTLT_App/migrations/0010_add_missing_attendance_fields.py
@@ -0,0 +1,40 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('PTLT_App', '0009_alter_attendancerecord_professor_and_more'),  # ← Your last migration
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='classschedule',
+            name='professor_time_in',
+        ),
+        migrations.RemoveField(
+            model_name='classschedule',
+            name='professor_time_out',
+        ),
+        migrations.AddField(
+            model_name='attendancerecord',
+            name='time_in',
+            field=models.TimeField(verbose_name='Time In', default='00:00'),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='attendancerecord',
+            name='time_out',
+            field=models.TimeField(verbose_name='Time Out', null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='attendancerecord',
+            name='professor_time_in',
+            field=models.TimeField(verbose_name='Professor Time In', null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='attendancerecord',
+            name='professor_time_out',
+            field=models.TimeField(verbose_name='Professor Time Out', null=True, blank=True),
+        ),
+    ]
