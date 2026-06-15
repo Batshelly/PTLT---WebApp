c9319198a443b71724cf47d58f5785179a44cef0
Batshelly <sjpotpot13@gmail.com>
2026-06-15 18:13:57 +0800

    Update middleware.py

--- PTLT_App/middleware.py
+++ PTLT_App/middleware.py
@@ -1,4 +1,7 @@
 from django.shortcuts import redirect
+import re
+from django.http import HttpResponseNotFound
+
 
 class SessionExpiredMiddleware:
     """
@@ -13,6 +16,21 @@ class SessionExpiredMiddleware:
             # If user is marked as authenticated but session is empty, session expired
             if not request.session.session_key:
                 return redirect('/?session_expired=true')
-        
+
         response = self.get_response(request)
         return response
+
+
+BLOCKED_PATHS = re.compile(
+    r'(/\.env|/\.git|.*\.env$|.*sendgrid|/wp-|/wordpress|/laravel)',
+    re.IGNORECASE
+)
+
+class BlockSensitivePathsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if BLOCKED_PATHS.search(request.path):
+            return HttpResponseNotFound()
+        return self.get_response(request)
